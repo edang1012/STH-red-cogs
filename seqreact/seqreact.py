be6e2f51f3abe370e96ce5ec3d32e821b0ab85dc
edang1012 <43113480+edang1012@users.noreply.github.com>
2019-10-04 22:26:10 -0400

    Update seqreact.py

--- seqreact/seqreact.py
+++ seqreact/seqreact.py
@@ -1,10 +1,10 @@
 from redbot.core import commands
 
-class Mycog(commands.Cog):
+class seqreact(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def test(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
