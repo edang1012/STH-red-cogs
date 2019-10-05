1b82fbe72c892daed6d2880a9ab10341f769eff0
edang1012 <43113480+edang1012@users.noreply.github.com>
2019-10-04 23:05:20 -0400

    Update seqreact.py

--- seqreact/seqreact.py
+++ seqreact/seqreact.py
@@ -4,18 +4,27 @@ class seqreact(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def test(self, ctx, word, num):
+    async def test(self, ctx, word, emoji):
+        
         """Create a sequence of reactions to a keyword/phrase'
-        Send the """
+        Usage:  pass a keyword or keyphrase(in quotations) to <word>
+                pass an emoji or emojis (in quotations) to <emoji>
+                
+                the order of emojis sent will be the sequence"""
+        
         guild = ctx.message.guild
         message = ctx.message
-        #await ctx.send(word)
-        #await ctx.send(num)
-        await self.create_reaction_sequence(guild, message, word, num)
+        await self.create_reaction_sequence(guild, message, word, emoji)
         
-    async def create_reaction_sequence(self, guild, message, word, num):
+        
+    async def create_reaction_sequence(self, guild, message, word, emoji):
         try:
-            await message.channel.send('Word is {}, num is {}'.format(word, num))
+            emotes = emoji.split(" ")
+            
+            for i in emotes
+                await message.channel.send(i)
+
+            #await message.channel.send('Word is {}, num is {}'.format(word, emoji))
 
         except (discord.errors.HTTPException, discord.errors.InvalidArgument):
             await message.channel.send("That's not an emoji I recognize. "
