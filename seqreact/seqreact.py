3ea547bf2537bef8a753b9671ede89ef06e4f32b
edang1012 <43113480+edang1012@users.noreply.github.com>
2019-10-04 23:20:54 -0400

    Update seqreact.py

--- seqreact/seqreact.py
+++ seqreact/seqreact.py
@@ -22,7 +22,7 @@ class seqreact(commands.Cog):
             emotes = emoji.split(" ")
             
             for x in emotes:
-                if ':xenpog:' in x:
+                if ':' in x:
                     await message.channel.send(x)
 
             #await message.channel.send('Word is {}, num is {}'.format(word, emoji))
