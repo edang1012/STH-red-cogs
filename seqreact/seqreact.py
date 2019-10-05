6d8173e8ed6ddf023e6f5a9682a87f9a1b140e60
edang1012 <43113480+edang1012@users.noreply.github.com>
2019-10-04 22:29:47 -0400

    Update seqreact.py

--- seqreact/seqreact.py
+++ seqreact/seqreact.py
@@ -4,7 +4,8 @@ class seqreact(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def test(self, ctx):
+    async def test(self, ctx, word, num):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send(word)
+        await ctx.send(num)
