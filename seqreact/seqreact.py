549857535f7f0d071d5088e5fa9498dd95d1721f
edang1012 <43113480+edang1012@users.noreply.github.com>
2019-10-04 22:44:54 -0400

    Update seqreact.py

--- seqreact/seqreact.py
+++ seqreact/seqreact.py
@@ -5,7 +5,18 @@ class seqreact(commands.Cog):
 
     @commands.command()
     async def test(self, ctx, word, num):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send(word)
-        await ctx.send(num)
+        """Create a sequence of reactions to a keyword/phrase'
+        Send the """
+        guild = ctx.message.guild
+        message = ctx.message
+        #await ctx.send(word)
+        #await ctx.send(num)
+        await self.create_reaction_sequence(guild, message, word, num)
+        
+    async def create_reaction_sequence(self, guild, message, word, num):
+    try:
+        await message.channel.send('Word is {}, num is {}'.format(word, num))
+
+    except (discord.errors.HTTPException, discord.errors.InvalidArgument):
+        await message.channel.send("That's not an emoji I recognize. "
+                                   "(might be custom!)")
