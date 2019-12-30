076b19a8939362a738ce2ea42426673319645ea3
edang1012 <43113480+edang1012@users.noreply.github.com>
2019-12-30 12:05:37 -0500

    Update jp2021money.py

--- jp2021money/jp2021money.py
+++ jp2021money/jp2021money.py
@@ -3,8 +3,18 @@ import discord
 from redbot.core import Config, commands, checks
 from redbot.core.utils.chat_formatting import pagify
 
-class jp2021money(commands.Cog):
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
+
+class jp2021money(commands.Cog):
+    """This bot reminds us about how much money we should have 
+        saved up for the Japan trip planned for May 2021."""
+    
     default_guild_settings = {
         #remove this when i figure out what this does
         "reactions": {}
@@ -20,8 +30,7 @@ class jp2021money(commands.Cog):
     @checks.mod_or_permissions(administrator=True)
     @commands.guild_only()
     @commands.command()
-    async def listseq(self, ctx):
+    async def test(self, ctx):
         """test command"""
         msg = "test command stuff"
         await ctx.send(msg)
-  
