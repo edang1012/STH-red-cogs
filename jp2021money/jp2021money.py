f28e2cf18c74ad6056e470896aebfd17f63150d3
edang1012 <43113480+edang1012@users.noreply.github.com>
2020-01-06 18:35:38 -0500

    Update jp2021money.py

--- jp2021money/jp2021money.py
+++ jp2021money/jp2021money.py
@@ -1,12 +1,28 @@
+# google sheets API stuff
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# redbot stuff
 import copy
 import discord
 from redbot.core import Config, commands, checks
 from redbot.core.utils.chat_formatting import pagify
 
+# google drive api from youtube
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
+# google sheets API
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SAMPLE_SPREADSHEET_ID = '1SOskKBFHYbVvdGIJ0ExGXCGYmSlajqUJDHHbVOlzGug'
+SAMPLE_RANGE_NAME = 'A2:D'
+
+# google drive API from youtube
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]         
 creds = ServiceAccountCredentials.from_json_keyfile_name("/home/pi/Bot_Archive/creds.json", scope)
 client = gspread.authorize(creds)
@@ -94,7 +110,7 @@ class jp2021money(commands.Cog):
     @checks.mod_or_permissions(administrator=True)
     @commands.guild_only()
     @commands.command()
-    async def testing(self, ctx):
+    async def testsheet(self, ctx):
         """Japan 2021 Trip Savings Reminder"""
         # Call the Sheets API
         sheet = client.open("Bot Money Saving Goals").sheet1
@@ -106,4 +122,50 @@ class jp2021money(commands.Cog):
         if col[0] == 'Week':
             msg = "I can talk to the sheet shishou!"
             await ctx.send(msg)
+
+
+
+
+
+    @checks.mod_or_permissions(administrator=True)
+    @commands.guild_only()
+    @commands.command()
+    async def test(self, ctx):    
+        """Shows basic usage of the Sheets API.
+        Prints values from a sample spreadsheet.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+        else:
+            print('Name, Major:')
+            for row in values:
+                # Print columns A and E, which correspond to indices 0 and 4.
+                print('%s, %s' % (row[0], row[3]))
 
