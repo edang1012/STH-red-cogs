1f2093f99e64a8bb4824dc11f957a7c58627d858
edang1012 <43113480+edang1012@users.noreply.github.com>
2020-01-06 19:45:59 -0500

    Update jp2021money.py

--- jp2021money/jp2021money.py
+++ jp2021money/jp2021money.py
@@ -147,7 +147,7 @@ class jp2021money(commands.Cog):
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
+                    '\home\pi\Downloads\credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
