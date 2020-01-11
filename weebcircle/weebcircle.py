eeb65dc9921597ac388373da0be5d566f4e8b596
edang1012 <43113480+edang1012@users.noreply.github.com>
2020-01-10 20:51:24 -0500

    Update weebcircle.py

--- weebcircle/weebcircle.py
+++ weebcircle/weebcircle.py
@@ -33,7 +33,7 @@ class weebcircle(commands.Cog):
     @commands.command()
     async def optin(self, ctx):
         self.circle.append(ctx.author.mention)
-        with open('/hom/pi/Bot_Archive/weeb_list.data') as f:
+        with open('/home/pi/Bot_Archive/weeb_list.data') as f:
             pickle.dump(self.circle,f)
         msg = "{} has been added to the circle".format(ctx.author.mention)
         await ctx.send(msg)
