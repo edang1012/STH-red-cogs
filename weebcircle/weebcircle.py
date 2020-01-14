6c986f306ec6b0dded206f55945a75de24772860
edang1012 <43113480+edang1012@users.noreply.github.com>
2020-01-14 16:07:40 -0500

    Update weebcircle.py

--- weebcircle/weebcircle.py
+++ weebcircle/weebcircle.py
@@ -134,13 +134,11 @@ class weebcircle(commands.Cog):
         with open('/home/pi/Bot_Archive/weeb_list.data', 'rb') as f:
             self.list = pickle.load(f)
             
-        #rand_list = np.array(self.list)
-        #old_list = np.array(self.list)
-        rand_list = self.list
-        old_list = self.list
+        rand_list = np.array(self.list)
+        old_list = np.array(self.list)
         
         while (rand_list[:,0] == old_list[:,0]).any():
-            random.shuffle(rand_list)
+            np.random.shuffle(rand_list)
             
         self.rand = rand_list
         self.list = old_list
