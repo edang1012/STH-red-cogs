c7016a2cf114ad637c51035e2ddaa7f71c5e8b68
edang1012 <43113480+edang1012@users.noreply.github.com>
2020-01-14 15:05:46 -0500

    Update weebcircle.py

--- weebcircle/weebcircle.py
+++ weebcircle/weebcircle.py
@@ -2,6 +2,7 @@ import discord
 import os
 import re
 import pickle
+import random
 
 from redbot.core import checks, Config, commands
 
@@ -20,6 +21,7 @@ class weebcircle(commands.Cog):
             **self.default_guild_settings
             )
         self.list = []
+        self.rand = []
         
 
     @commands.guild_only()
@@ -35,8 +37,9 @@ class weebcircle(commands.Cog):
         """Usage: Enter the number of cours you would like to watch
         Input any number of cours or specify with the keywords below:
         1 cour: 1, Easy, Wolf
-        2 cour: 2, Med, Medium, Tiger
-        3 cour: 3, Hard, Demon"""
+        2 cours: 2, Med, Medium, Tiger
+        3 cours: 3, Hard, Demon
+        3+ cours: Expert, Dragon, Ryu"""
         
         if any(ctx.author.mention in list for list in self.list):
             msg = "You are already in the list baka"
@@ -46,28 +49,31 @@ class weebcircle(commands.Cog):
                 self.list.append([ctx.author.mention, arg1])
                 with open('/home/pi/Bot_Archive/weeb_list.data', 'wb') as f:
                     pickle.dump(self.list,f)
-                msg = "{} has been added to the list and wants {} cour(s).".format(ctx.author.mention,arg1)
+                msg = "{} has been added to the list and wants at most {} cour(s).".format(ctx.author.mention,arg1)
             
             elif (arg1.lower() == 'easy') or (arg1.lower() == 'wolf') or (arg1.lower() == 'okami'):
                 self.list.append([ctx.author.mention, '1'])
                 with open('/home/pi/Bot_Archive/weeb_list.data', 'wb') as f:
                     pickle.dump(self.list,f)
-                msg = "{} has been added to the list and wants 1 cour.".format(ctx.author.mention)
+                msg = "{} has been added to the list and wants at most 1 cour.".format(ctx.author.mention)
             
             elif (arg1.lower() == 'med') or (arg1.lower() == 'medium') or (arg1.lower() == 'tiger') or (arg1.lower() == 'tora'):
                 self.list.append([ctx.author.mention, '2'])
                 with open('/home/pi/Bot_Archive/weeb_list.data', 'wb') as f:
                     pickle.dump(self.list,f)
-                msg = "{} has been added to the list and wants 2 cours.".format(ctx.author.mention)
+                msg = "{} has been added to the list and wants at most 2 cours.".format(ctx.author.mention)
                 
             elif (arg1.lower() == 'hard') or (arg1.lower() == 'demon') or (arg1.lower() == 'oni'):
                 self.list.append([ctx.author.mention, '3+'])
                 with open('/home/pi/Bot_Archive/weeb_list.data', 'wb') as f:
                     pickle.dump(self.list,f)
-                msg = "{} has been added to the list and wants 3+ cours.".format(ctx.author.mention)
+                msg = "{} has been added to the list and wants at most 3 cours.".format(ctx.author.mention)
             
-            elif (arg1.lower() == 'dragon') or (arg1.lower() == 'ryu'):
-                msg = "Just watch One Piece or Detective Conan, you don't need this. Fucking masochist..."
+            elif (arg1.lower() == 'expert') or (arg1.lower() == 'dragon') or (arg1.lower() == 'ryu'):
+                self.list.append([ctx.author.mention, '3+'])
+                with open('/home/pi/Bot_Archive/weeb_list.data', 'wb') as f:
+                    pickle.dump(self.list,f)
+                msg = "{} has been added to the list and wants 3+ cours.".format(ctx.author.mention)
             
             elif (arg1.lower() == 'god') or (arg1.lower() == 'kami'):
                 self.list.append([ctx.author.mention, '3+'])
@@ -131,4 +137,22 @@ class weebcircle(commands.Cog):
         with open('/home/pi/Bot_Archive/weeb_list.data', 'wb') as f:
             pickle.dump(self.list,f)
         msg = "The list has been cleared"
+        await ctx.send(msg)
+        
+    @commands.guild_only()
+    @checks.admin_or_permissions(manage_guild=True)
+    @commands.command()
+    async def randomize(self, ctx):
+        self.rand = self.list
+        random.shuffle(self.rand)
+        
+        msg = "Not Rand:\n"
+        for member in self.list:
+            msg += "{} wants to watch ".format(member[0])
+            msg += "{} cour(s)\n".format(member[1])
+            
+        msg += "\n\n Rand:\n"
+        for member in self.rand:
+            msg += "{} wants to watch ".format(member[0])
+            msg += "{} cour(s)\n".format(member[1])
         await ctx.send(msg)
