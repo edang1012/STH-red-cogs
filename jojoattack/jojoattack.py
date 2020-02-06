7f35c470baa924db84c6c1798a5ec63cf2f1c753
adang2074 <60586994+adang2074@users.noreply.github.com>
2020-02-06 09:04:48 -0500

    Update jojoattack.py

--- jojoattack/jojoattack.py
+++ jojoattack/jojoattack.py
@@ -29,12 +29,20 @@ class jojoattack(BaseCog):
     async def jojoattack(self, ctx):
         """Use a randomized JoJo attack!"""
         
-        r = random.randint(0, 10)
+        r = random.randint(0, 1)
         
         if r == 0
             embed = discord.Embed(
                 description = 'ORA ORA ORA ORA!!!',
                 color = discord.Color.red()
             )
-            embed.set_image(url='')
+            embed.set_image(url='https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fora-ora-ora-gifs&psig=AOvVaw3KISyXyEUb4EzCJjROAkgr&ust=1581084141702000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPCYi8eLvecCFQAAAAAdAAAAABAD')
+            await ctx.send(embed=embed)
+         
+        if r == 1
+            embed = discord.Embed(
+                description = 'MUDA MUDA MUDA MUDA!!!',
+                color = discord.Color.red()
+            )
+            embed.set_image(url='https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fview%2Fmuda-the-world-punch-jo-jos-bizarre-adventure-gif-12851143&psig=AOvVaw3NDGv85B6GMymokmyGn1xg&ust=1581084259673000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCID3rv-LvecCFQAAAAAdAAAAABAD')
             await ctx.send(embed=embed)
