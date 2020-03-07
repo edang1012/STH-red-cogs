f5e7fe0568fc55aaae87256663fd4cf9ecdc0d59
edang1012 <43113480+edang1012@users.noreply.github.com>
2020-03-07 16:10:17 -0500

    Update weebcircle.py

--- weebcircle/weebcircle.py
+++ weebcircle/weebcircle.py
@@ -421,7 +421,7 @@ class weebcircle(commands.Cog):
 
             for member in self.list:
                 if len(member) < 4:
-                    msg = "{} needs to recommend something using **.rec**...".format(member[0])
+                    msg = "{} needs to recommend something using **.rec**...".format(member[2])
                     await ctx.send(msg)
 
             if msg == "none":                
